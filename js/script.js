ec32e57724f31a1eb1d8c742252d1dc3c6975215
planetmc <filyy5040@gmail.com>
2025-06-20 15:15:36 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ If something doesn't work please contact me on discord (Astronawta#0012).
 
 const config = {
     serverInfo: {
-        serverLogoImageFileName: "pianet.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "PlanetMC", /*Server name*/
         serverIp: "planetmc.kozow.com", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
         discordServerID: "489529070913060867" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
